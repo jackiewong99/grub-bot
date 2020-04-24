23b3e06811ffda546830c060c53ad045e8638201
Jackie Wong <wongjackie888@gmail.com>
2020-04-23 20:11:53 -1000

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,17 @@
+// Test fetching data from Yelp API
+
+// All functionality goes in this file temporarily until first version project is finished
+
+const API_ADDRESS = 'https://api.yelp.com/v3';
+
+// ************    TEST                 *******************************
+// var messages = document.querySelector('.messages');
+// var textbox = document.querySelector('.textbox');
+// var sendBtn = document.querySelector('.send');
+
+// sendBtn.addEventListener('click', function() {
+//   var newMessage = document.createElement('li');
+//   newMessage.innerHTML = textbox.value;
+//   messages.appendChild(newMessage);
+//   textbox.value = '';
+// });
