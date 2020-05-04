dc6207ed156e369fe4f7a458ebcdb693bbd22833
Jackie Wong <wongjackie888@gmail.com>
2020-05-03 20:16:03 -1000

    Setup proxy server and test fetching
    Setup proxy server for Yelp API and test fetching data from the front-end

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,17 +1,75 @@
+// ************************************************************************************************************************
+
 // Test fetching data from Yelp API
 
 // All functionality goes in this file temporarily until first version project is finished
 
-const API_ADDRESS = 'https://api.yelp.com/v3';
+// Local Proxy ADDRESS: http://localhost:3000/businesses/offset/latitude/longitude
 
-// ************    TEST                 *******************************
-// var messages = document.querySelector('.messages');
-// var textbox = document.querySelector('.textbox');
-// var sendBtn = document.querySelector('.send');
+// *** EXAMPLE: ***
 
-// sendBtn.addEventListener('click', function() {
-//   var newMessage = document.createElement('li');
-//   newMessage.innerHTML = textbox.value;
-//   messages.appendChild(newMessage);
-//   textbox.value = '';
-// });
+// let resultOffset = 101;
+// let latitude = '21.2869027';
+// let longitude = '-157.7973769';
+// fetch(`${PROXY_ADDRESS}/${resultOffset}/${latitude}/${longitude}`)
+//   .then((response) => response.json())
+//   .then((json) => {
+//     let businesses = json.businesses;
+//     console.log(businesses);
+//   });
+
+// ****************
+
+// ************************************************************************************************************************
+
+const PROXY_ADDRESS = 'http://localhost:3000/businesses';
+
+const cuisines = [
+  {
+    set: '1',
+    cuisineSet: ['American', 'Asian fusion', 'Cantonese', 'Chinese'],
+  },
+  {
+    set: '2',
+    cuisineSet: ['French', 'Greek', 'Indian', 'Italian'],
+  },
+  {
+    set: '3',
+    cuisineSet: ['Japanese', 'Korean', 'Mediterranean', 'Mexican'],
+  },
+  {
+    set: '4',
+    cuisineSet: ['Middle Eastern', 'Moroccan', 'Spanish'],
+  },
+  {
+    set: '5',
+    cuisineSet: ['Taiwanese', 'Thai', 'Vegan', 'Vegetarian'],
+  },
+];
+
+function init() {
+  let chat = document.querySelector('.container-chat-msg');
+  let initMsg = document.createElement('div');
+  let initPrompt = document.createElement('div');
+  let welcomeMsg = 'Welcome to Grub Bot!';
+  let promptMsg = 'Start by selecting a cuisine of choice below. (Pick one)';
+
+  initMsg.classList.add('chat-msg', 'chat-msg--left');
+  initPrompt.classList.add('chat-msg', 'chat-msg--left');
+  initMsg.innerHTML = welcomeMsg;
+  initPrompt.innerHTML = promptMsg;
+
+  chat.appendChild(initMsg);
+  chat.appendChild(initPrompt);
+}
+
+function cuisineChoices() {}
+
+function removeElement(elementId) {
+  let element = document.querySelector(elementId);
+  element.parentNode.removeElement(element);
+}
+
+window.onload = function () {
+  setTimeout(init, 750);
+};
--- server/index.js
+++ server/index.js
@@ -1,7 +1,62 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const fetch = require('node-fetch');
 
 require('dotenv').config();
 
 const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+
+// Authorization Headers for Requests
+
+let myHeaders = {
+  Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+  Cookie: '__cfduid=da1a9499ce24388795e03e73d3c00340b1587106186',
+};
+
+let requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow',
+};
+
+// let coordinates = '&latitude=21.2869027&longitude=-157.7973769';
+
+// Routes
+
+app.get('/businesses/:offset/:latitude/:longitude', (req, res) => {
+  let coordinates = `&latitude=${req.params.latitude}&longitude=${req.params.longitude}`;
+  let resultOffset = `&offset=${req.params.offset}`;
+  const API_ADDRESS = `https://api.yelp.com/v3/businesses/search?limit=50${coordinates}${resultOffset}`;
+  fetch(API_ADDRESS, requestOptions)
+    .then((response) => response.json())
+    .then((json) => {
+      res.json(json);
+      let businesses = json.businesses;
+      console.log(businesses.length);
+    });
+});
+
+function notFound(req, res, next) {
+  const error = new Error('Not Found');
+  res.status(404);
+  next(error);
+}
+
+function errorHandler(error, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: error.message,
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
