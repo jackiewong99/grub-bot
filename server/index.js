bf324e1627c2aaf6230c37cc6c06d6dff4c9624a
Jackie Wong <wongjackie888@gmail.com>
2020-05-11 21:28:05 -1000

    Remove parentheses around parameter of callback functions.

--- server/index.js
+++ server/index.js
@@ -14,13 +14,13 @@ app.use(cors());
 
 let myHeaders = {
   Authorization: `Bearer ${process.env.YELP_API_KEY}`,
-  Cookie: '__cfduid=da1a9499ce24388795e03e73d3c00340b1587106186',
+  Cookie: '__cfduid=da1a9499ce24388795e03e73d3c00340b1587106186'
 };
 
 let requestOptions = {
   method: 'GET',
   headers: myHeaders,
-  redirect: 'follow',
+  redirect: 'follow'
 };
 
 // let coordinates = '&latitude=21.2869027&longitude=-157.7973769';
@@ -32,8 +32,8 @@ app.get('/businesses/:offset/:latitude/:longitude', (req, res) => {
   let resultOffset = `&offset=${req.params.offset}`;
   const API_ADDRESS = `https://api.yelp.com/v3/businesses/search?limit=50${coordinates}${resultOffset}`;
   fetch(API_ADDRESS, requestOptions)
-    .then((response) => response.json())
-    .then((json) => {
+    .then(response => response.json())
+    .then(json => {
       res.json(json);
       let businesses = json.businesses;
       console.log(businesses.length);
@@ -49,7 +49,7 @@ function notFound(req, res, next) {
 function errorHandler(error, req, res, next) {
   res.status(res.statusCode || 500);
   res.json({
-    message: error.message,
+    message: error.message
   });
 }
 
