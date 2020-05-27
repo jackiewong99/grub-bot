cd6f77368808b49512ffe9404fb8c08418e060a8
Jackie Wong <wongjackie888@gmail.com>
2020-05-26 21:41:52 -1000

    Place user preference filtering functions in a separate file.

---
+++ dist/js/filterPref.js
@@ -0,0 +1,71 @@
+// Filter for user's cuisine preference
+export async function filterCuisine(results) {
+  const cuisinePref = [];
+
+  results.forEach(result => {
+    result.categories.forEach(category => {
+      if (category.title === userPref[0].cuisine) {
+        cuisinePref.push(result);
+      }
+    });
+  });
+
+  return cuisinePref;
+}
+
+// Filter for user's budget preference
+export async function filterBudget(results) {
+  const budgetPref = results.filter(
+    result => result.price === userPref[1].budget
+  );
+
+  return budgetPref;
+}
+
+// Filter for nearby restaurants (within 2 miles)
+export async function filterDistance(results) {
+  const distPref = results.filter(result => result.distance <= 3218.69);
+
+  return distPref;
+}
+
+// Filter for high review count and rating
+export async function filterFame(results) {
+  const reviews = results.filter(result => result.review_count > 1000);
+  const rating = reviews.filter(business => business.rating >= 3.5);
+
+  return rating;
+}
+
+/*
+Final check for user's combination of answers for 
+preferring a nearby restaurant and if
+they prefer high review count and rating
+*/
+export async function checkPreferences(
+  budgetPref,
+  distPref,
+  famePref,
+  famePrefOnly
+) {
+  const finalFilter = [];
+  if (userPref[2].prefNearby === 'Yes' && userPref[3].fame === 'Yes') {
+    famePref.forEach(business => {
+      finalFilter.push(business);
+    });
+  } else if (userPref[2].prefNearby === 'Yes' && userPref[3].fame === 'No') {
+    distPref.forEach(business => {
+      finalFilter.push(business);
+    });
+  } else if (userPref[2].prefNearby === 'No' && userPref[3].fame === 'Yes') {
+    famePrefOnly.forEach(business => {
+      finalFilter.push(business);
+    });
+  } else if (userPref[2].prefNearby === 'No' && userPref[3].fame === 'No') {
+    budgetPref.forEach(business => {
+      finalFilter.push(business);
+    });
+  }
+
+  return finalFilter;
+}
