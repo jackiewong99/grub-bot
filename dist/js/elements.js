27fd8c60b7a71d103f45667534e2dea97b8e0143
Jackie Wong <wongjackie888@gmail.com>
2020-06-02 01:05:36 -1000

    Refactor and clean up code for 'showRestaurant'.
    Place each creation of restaurant's information of HTML elements into separate functions.

--- dist/js/elements.js
+++ dist/js/elements.js
@@ -53,7 +53,41 @@ export function showRestaurant(restaurant) {
   const resCard = document.createElement('div');
   resCard.classList.add('card');
 
-  // Create 'div' container to hold restaurant's name
+  // Get restaurant's information
+  const resNameContainer = getResName(restaurant);
+  const imgContainer = getResImg(restaurant);
+  const categoryContainer = getResCategory(restaurant);
+  const priceContainer = getResPrice(restaurant);
+  const ratingContainer = getResRating(restaurant);
+  const distContainer = getResDistance(restaurant);
+  const addressContainer = getResAddress(restaurant);
+  const phoneNumContainer = getResPhoneNum(restaurant);
+  const yelpRefContainer = getYelpRef(restaurant);
+
+  // Create 'div' container to hold all restaurant information
+  const infoContainer = document.createElement('div');
+  infoContainer.classList.add('card-info');
+  infoContainer.appendChild(categoryContainer);
+  infoContainer.appendChild(priceContainer);
+  infoContainer.appendChild(ratingContainer);
+  infoContainer.appendChild(distContainer);
+  infoContainer.appendChild(addressContainer);
+  infoContainer.appendChild(phoneNumContainer);
+  infoContainer.appendChild(yelpRefContainer);
+
+  // Append all restaurant elements to the main
+  // Restaurant card container and display to user
+  resCard.appendChild(resNameContainer);
+  resCard.appendChild(imgContainer);
+  resCard.appendChild(infoContainer);
+  cardContainer.appendChild(resCard);
+
+  const body = document.querySelector('.home-view');
+  body.appendChild(cardContainer);
+}
+
+// Create 'div' container to hold restaurant's name
+function getResName(restaurant) {
   const resNameContainer = document.createElement('div');
   const resName = document.createElement('a');
   resName.innerHTML = restaurant.name;
@@ -62,16 +96,22 @@ export function showRestaurant(restaurant) {
   resName.classList.add('card-name');
   resNameContainer.classList.add('card-name-container');
   resNameContainer.appendChild(resName);
+  return resNameContainer;
+}
 
-  // Create 'div' container to hold restaurant's image
+// Create 'div' container to hold restaurant's image
+function getResImg(restaurant) {
   const imgContainer = document.createElement('div');
   const resImg = document.createElement('img');
   resImg.src = restaurant.image_url;
   resImg.classList.add('card-img');
   imgContainer.classList.add('card-img-container');
   imgContainer.appendChild(resImg);
+  return imgContainer;
+}
 
-  // Create 'div' container to hold categories of restaurant
+// Create 'div' container to hold categories of restaurant
+function getResCategory(restaurant) {
   const categoryContainer = document.createElement('div');
   const categories = restaurant.categories;
   const categoryHeader = document.createElement('span');
@@ -102,8 +142,12 @@ export function showRestaurant(restaurant) {
   }
   categoryContainer.classList.add('card-categories-container');
 
-  // Create 'div' container to hold restaurant's rating
-  // Show rating icon based on restaurant's rating
+  return categoryContainer;
+}
+
+// Create 'div' container to hold restaurant's rating
+// Show rating icon based on restaurant's rating
+function getResPrice(restaurant) {
   const priceContainer = document.createElement('div');
   const priceHeader = document.createElement('p');
   const price = document.createElement('p');
@@ -115,23 +159,62 @@ export function showRestaurant(restaurant) {
   priceContainer.appendChild(priceHeader);
   priceContainer.appendChild(price);
 
-  // Create 'div' container to hold restaurant's rating
-  const ratingContainer = document.createElement('div');
-  const ratingHeader = document.createElement('p');
-  const rating = document.createElement('p');
-  const ratingKeyWord = document.createElement('p');
-  ratingHeader.innerHTML = 'Rating:';
-  ratingHeader.classList.add('card-rating-header');
-  ratingKeyWord.innerHTML = 'stars';
-  ratingKeyWord.classList.add('card-rating');
-  rating.innerHTML = restaurant.rating;
-  rating.classList.add('card-rating');
-  ratingContainer.classList.add('card-rating-container');
-  ratingContainer.appendChild(ratingHeader);
-  ratingContainer.appendChild(rating);
-  ratingContainer.appendChild(ratingKeyWord);
+  return priceContainer;
+}
 
-  // Create 'div' container to hold user's distance to the restaurant
+// Create 'div' container to hold restaurant's rating
+function getResRating(restaurant) {
+  const ratingContainer = document.createElement('div');
+  const rating = document.createElement('img');
+  switch (restaurant.rating) {
+    case 0:
+      rating.src = './icons/small/small_0.png';
+      break;
+
+    case 1:
+      rating.src = './icons/small/small_1.png';
+      break;
+
+    case 1.5:
+      rating.src = './icons/small/small_1_half.png';
+      break;
+
+    case 2:
+      rating.src = './icons/small/small_2.png';
+      break;
+
+    case 2.5:
+      rating.src = './icons/small/small_2_half.png';
+      break;
+
+    case 3:
+      rating.src = './icons/small/small_3.png';
+      break;
+
+    case 3.5:
+      rating.src = './icons/small/small_3_half.png';
+      break;
+
+    case 4:
+      rating.src = './icons/small/small_4.png';
+      break;
+
+    case 4.5:
+      rating.src = './icons/small/small_4_half.png';
+      break;
+
+    case 5:
+      rating.src = './icons/small/small_5.png';
+      break;
+  }
+  ratingContainer.classList.add('card-rating-container');
+  ratingContainer.appendChild(rating);
+
+  return ratingContainer;
+}
+
+// Create 'div' container to hold user's distance to the restaurant
+function getResDistance(restaurant) {
   const distContainer = document.createElement('div');
   const distHeader = document.createElement('p');
   const getDistance = restaurant.distance / 1609.344;
@@ -150,7 +233,11 @@ export function showRestaurant(restaurant) {
   distContainer.appendChild(distHeader);
   distContainer.appendChild(distanceText);
 
-  // Create 'div' container to hold restaurant's address
+  return distContainer;
+}
+
+// Create 'div' container to hold restaurant's address
+function getResAddress(restaurant) {
   const addressContainer = document.createElement('div');
   const addressHeader = document.createElement('p');
   const address = restaurant.location.display_address;
@@ -165,7 +252,11 @@ export function showRestaurant(restaurant) {
   });
   addressContainer.classList.add('card-address-container');
 
-  // Create 'div' container to hold restaurant's phone number
+  return addressContainer;
+}
+
+// Create 'div' container to hold restaurant's phone number
+function getResPhoneNum(restaurant) {
   const phoneNumContainer = document.createElement('div');
   const phoneNumHeader = document.createElement('p');
   const phoneNum = document.createElement('p');
@@ -178,7 +269,11 @@ export function showRestaurant(restaurant) {
   phoneNumContainer.appendChild(phoneNumHeader);
   phoneNumContainer.appendChild(phoneNum);
 
-  // Create 'div' container to display referral to restaurant's Yelp page
+  return phoneNumContainer;
+}
+
+// Create 'div' container to display referral to restaurant's Yelp page
+function getYelpRef(restaurant) {
   const yelpRefContainer = document.createElement('div');
   const yelpRef = document.createElement('a');
   yelpRef.innerHTML = 'For more information, view their page on Yelp! ->';
@@ -188,24 +283,5 @@ export function showRestaurant(restaurant) {
   yelpRefContainer.classList.add('card-ref-container');
   yelpRefContainer.appendChild(yelpRef);
 
-  // Create 'div' container to hold all restaurant information
-  const infoContainer = document.createElement('div');
-  infoContainer.classList.add('card-info');
-  infoContainer.appendChild(categoryContainer);
-  infoContainer.appendChild(priceContainer);
-  infoContainer.appendChild(ratingContainer);
-  infoContainer.appendChild(distContainer);
-  infoContainer.appendChild(addressContainer);
-  infoContainer.appendChild(phoneNumContainer);
-  infoContainer.appendChild(yelpRefContainer);
-
-  // Append all restaurant elements to the main
-  // Restaurant card container
-  resCard.appendChild(resNameContainer);
-  resCard.appendChild(imgContainer);
-  resCard.appendChild(infoContainer);
-  cardContainer.appendChild(resCard);
-
-  const body = document.querySelector('.home-view');
-  body.appendChild(cardContainer);
+  return yelpRefContainer;
 }
